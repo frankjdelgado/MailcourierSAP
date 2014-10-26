2366eaf46cba9c9de8289b80cc7d9630fe6b8a32
Francisco Delgado <fdelgado@4geeks.com.ve>
2014-10-26 16:57:54 -0430

    templates

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,15 @@
 // app.js
+
+// Define module
 var app = angular.module('mcapp', ['ui.router']);
+
+// Config routes
+app.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$stateProvider) {
+	// All requests to non defined enpoints go to root
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider.
+		state('home', {
+			templateUrl: 'home.html'
+		})
+}]);
