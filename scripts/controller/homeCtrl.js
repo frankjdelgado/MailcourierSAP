519ac929b7fecd0fffe3d7c35497337329c63409
Frank <frankjdelgado@gmail.com>
2014-10-27 14:20:07 -0430

    home template

--- scripts/controller/homeCtrl.js
+++ scripts/controller/homeCtrl.js
@@ -2,4 +2,18 @@ angular
 	.module('mcapp')
 	.controller('homeCtrl', ['$scope', function($scope){
 		$scope.title = "Welcome!";
+		
+		// Calculator
+		$scope.calculate = function(){
+
+			$http.get('',{
+				'email': that.user.email,
+				'password': that.user.password
+			})
+			.success(function(data){
+				localStorageService.set('login_token',data.login_token);
+				localStorageService.set('id',data.id);
+			})
+		};
+
 	}]);
