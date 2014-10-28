6aa823bd87b26f89b7690d011fcd9b856753d5f0
Frank <frankjdelgado@gmail.com>
2014-10-27 23:18:07 -0430

    public calculate

--- scripts/controller/homeCtrl.js
+++ scripts/controller/homeCtrl.js
@@ -1,19 +1,27 @@
 angular
 	.module('mcapp')
-	.controller('homeCtrl', ['$scope', function($scope){
+	.controller('homeCtrl', ['$scope','$http', function($scope,$http){
 		$scope.title = "Welcome!";
 		
 		// Calculator
 		$scope.calculate = function(){
 
-			$http.get('',{
-				'email': that.user.email,
-				'password': that.user.password
+			var url = 'http://0.0.0.0:3000/api/v1/rate/calculate';
+			$http.get(url,{
+				params:{
+					height: this.package.height,
+					width: this.package.width,
+					depth: this.package.depth,
+					weight: this.package.weight,
+					value: this.package.value
+				}
 			})
 			.success(function(data){
-				localStorageService.set('login_token',data.login_token);
-				localStorageService.set('id',data.id);
+				$scope.shipping_cost = data.shipping_cost;
 			})
+			.error(function(data){
+			
+			});
 		};
-
 	}]);
+	
