10dc3bc27d49dccac595cfe0f48906641c4d2e47
Francisco Delgado <fdelgado@4geeks.com.ve>
2014-11-08 18:27:35 -0430

    show/hide menu options after login

--- scripts/controller/sessionCtrl.js
+++ scripts/controller/sessionCtrl.js
@@ -4,24 +4,25 @@ angular
 
 		var url = "http://0.0.0.0:3000/api/v1/session";
 		
-		// User auth. Get token, id, role and redirect to /packages
+		// User auth. Get token, username, role and redirect to /packages
 		$scope.login = function(){
 			$http({
 				method: 'POST',
 				url: url,
 				headers:{
-					'username': $scope.username,
-					'password': $scope.password
+					'username': $scope.user.username,
+					'password': $scope.user.password
 				}
 			})
 			.success(function(data,status,headers,config){
 				if(localStorageService.isSupported){
 
 					localStorageService.set('token', data.token);
-					localStorageService.set('id', data.id);
+					localStorageService.set('username', data.username);
 					localStorageService.set('role', data.role);
-					$location.path("/packages");
 					
+					$location.path("/");
+
 				}else{
 					alert("Your browser does not support localStorage");
 				}
@@ -31,10 +32,13 @@ angular
 			});
 		};
 
+
+		$scope.username = localStorageService.get('username');
+
 		// User logout. Destroy localStroage and redirect to home
 		$scope.logout = function() {
 			localStorageService.remove("token");
-			localStorageService.remove("id");
+			localStorageService.remove("username");
 			localStorageService.remove("role");
 			$location.path("/");
 		};
@@ -42,5 +46,20 @@ angular
 		// Check if user is logged
 		$scope.userIsLogged = function(){
 			return (localStorageService.get('token'))? true : false;
+		};
+
+		// Check if user is admin
+		$scope.isAdmin = function(){
+			return (localStorageService.get('role') == '2')? true : false;
+		};
+
+		// Check if user is operator
+		$scope.isOperator = function(){
+			return (localStorageService.get('role') == '1')? true : false;
+		};
+
+		// Check if user is member
+		$scope.isMember = function(){
+			return (localStorageService.get('role') === '0')? true : false;
 		};
 	}]);
