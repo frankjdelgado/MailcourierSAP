ba9ee21ba58911e16f5f5c87eba778d7c4f997f6
Francisco Delgado <fdelgado@4geeks.com.ve>
2014-11-08 21:18:26 -0430

    filters for login and private urls

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@
 var app = angular.module('mcapp', ['ui.router','LocalStorageModule']);
 
 // Config routes
-app.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$stateProvider) {
+app.config(['$urlRouterProvider','$stateProvider','$locationProvider',function($urlRouterProvider, $stateProvider, $locationProvider) {
 	// All requests to non defined enpoints go to root
 	$urlRouterProvider.otherwise('/');
 
@@ -26,7 +26,40 @@ app.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$s
 			controler: 'packageCtrl',
 			templateUrl: 'templates/packages/index.html'
 		});
+
+	// Get rid of /#/ on routes
+	// $locationProvider.html5Mode(true);
+
 }]);
+
+
+// Global $scope
+app.run(['$rootScope','localStorageService','$location', function($rootScope, localStorageService, $location){
+
+	// Global functions
+	$rootScope.isLogged = function(){
+		return (localStorageService.get('token'))? true : false;
+	};
+
+	// Route filters
+	$rootScope.$on("$stateChangeStart",function (event, toState, toParams, fromState, fromParams) {
+
+		// Restrict Login when user is already logged
+		if($rootScope.isLogged() && toState.name === 'login'){
+			event.preventDefault();
+			$location.path('/');
+		}
+
+		// Restrict all private URLs for not authorized users
+		if(!$rootScope.isLogged() && ['home','login', 'signup'].indexOf(toState.name) === -1 ){
+			event.preventDefault();
+			$location.path('/');
+		}
+
+	});
+	
+}]);
+
 
 // Config localstorage
 app.config(function (localStorageServiceProvider) {
